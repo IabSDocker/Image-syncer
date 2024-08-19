afa59e391bc548ce4fbe185b38b7682938583914
IabSDocker <llswdhh@163.com>
2024-08-19 11:26:30 +0800

    添加namespce字段，自定义本地镜像保存tag

--- utils/config.go
+++ utils/config.go
@@ -1,26 +1,57 @@
+// package utils
+
+// import (
+//     "io/ioutil"
+//     "gopkg.in/yaml.v2"
+// )
+
+// type Config struct {
+//     SourceRepo string   `yaml:"source_repo"`
+//     TargetRepo string   `yaml:"target_repo"`
+//     Images     []string `yaml:"images"`
+// }
+
+// func LoadConfig(path string) (*Config, error) {
+//     var config Config
+//     data, err := ioutil.ReadFile(path)
+//     if err != nil {
+//         return nil, err
+//     }
+//     err = yaml.Unmarshal(data, &config)
+//     if err != nil {
+//         return nil, err
+//     }
+//     return &config, nil
+// }
+
 package utils
 
 import (
-    "io/ioutil"
     "gopkg.in/yaml.v2"
+    "os"
 )
 
 type Config struct {
     SourceRepo string   `yaml:"source_repo"`
     TargetRepo string   `yaml:"target_repo"`
+    Namespace  string   `yaml:"namespace"`
     Images     []string `yaml:"images"`
 }
 
-func LoadConfig(path string) (*Config, error) {
+func LoadConfig(filePath string) (*Config, error) {
     var config Config
-    data, err := ioutil.ReadFile(path)
+    file, err := os.Open(filePath)
     if err != nil {
         return nil, err
     }
-    err = yaml.Unmarshal(data, &config)
+    defer file.Close()
+    
+    decoder := yaml.NewDecoder(file)
+    err = decoder.Decode(&config)
     if err != nil {
         return nil, err
     }
+    
     return &config, nil
 }
 
